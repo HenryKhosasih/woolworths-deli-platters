40911bcb32b343d40e36960f6d379fbe1af4f0ab
Henry Laurent Khosasih <henrykhosasih@TrickyPanda.local>
2023-05-12 22:54:14 +1000

    enable scroll to top button

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import Image from "next/image";
+import StraightIcon from "@mui/icons-material/Straight";
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
     <div className="w-full bg-[#171c1f] flex p-8 justify-between">
       <Image
@@ -9,8 +15,13 @@ const Footer = () => {
         height={50}
         alt="woolworths-logo"
       />
-      <button className="bg-[#24292C] text-[#fff] text-sm rounded-lg p-4">
+
+      <button
+        className="bg-[#24292C] text-[#fff] text-sm rounded-lg p-4 hover:bg-[#373e43]"
+        onClick={scrollToTop}
+      >
         Back to top
+        <StraightIcon />
       </button>
     </div>
   );
