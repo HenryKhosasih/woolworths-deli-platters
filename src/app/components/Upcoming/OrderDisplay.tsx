e4377cf72dfb6f8ca06d0d3344da205c7e1f6ec7
Henry Laurent Khosasih <henrykhosasih@TrickyPanda.local>
2023-05-20 18:55:54 +1000

    update locale date format to au

--- src/app/components/Upcoming/OrderDisplay.tsx
+++ src/app/components/Upcoming/OrderDisplay.tsx
@@ -24,7 +24,8 @@ const OrderDisplay = ({
             </h1>
             <p className="p-4 pb-10 border-b border-b-[#E0E0E0]">
               Delete {name}&apos;s order on{" "}
-              {new Date(toEpochMilli(pickupDate)).toLocaleDateString()}? <br />
+              {new Date(toEpochMilli(pickupDate)).toLocaleDateString("en-au")}?{" "}
+              <br />
               This action cannot be reversed.
             </p>
             <div className="flex p-2 justify-end gap-4">
@@ -69,7 +70,7 @@ const OrderDisplay = ({
     <div key={id} className="flex p-4 space-x-5 sm:space-x-10 bg-primarylight">
       <div className="flex flex-col m-auto">
         <p className="text-center text-lg">
-          {new Date(toEpochMilli(pickupDate)).toLocaleDateString()}
+          {new Date(toEpochMilli(pickupDate)).toLocaleDateString("en-au")}
         </p>
         <p className="text-center text-lg">{pickupTime}</p>
         <p className="text-center text-xs">({name})</p>
